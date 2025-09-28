18172c6cecbbb4807272771d1754fb2c0a33049b
Queona Reyes <sailorchu2020@gmail.com>
2025-09-28 10:56:38 -0700

    backend

--- weather-backend/server.js
+++ weather-backend/server.js
@@ -6,7 +6,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5500;
+const PORT = process.env.PORT || 3000;
 const API_KEY = process.env.WEATHER_API_KEY;
 
 app.use(express.json());
