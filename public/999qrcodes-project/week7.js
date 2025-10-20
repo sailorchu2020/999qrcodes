000b56c2b030409301973a67b9bb26977a0c75c7
Queona Reyes <sailorchu2020@gmail.com>
2025-10-19 22:01:40 -0700

    Week 7

---
+++ public/999qrcodes-project/week7.js
@@ -0,0 +1,65 @@
+function playC4() {
+  let audio = document.getElementById("C4");
+  audio.currentTime = 0;
+  audio.play();
+}
+function playCsh4() {
+  let audio = document.getElementById("Csh4");
+  audio.currentTime = 0;
+  audio.play();
+}
+function playD4() {
+  let audio = document.getElementById("D4");
+  audio.currentTime = 0;
+  audio.play();
+}
+function playEb4() {
+  let audio = document.getElementById("Eb4");
+  audio.currentTime = 0;
+  audio.play();
+}
+function playE4() {
+  let audio = document.getElementById("E4");
+  audio.currentTime = 0;
+  audio.play();
+}
+function playF4() {
+  let audio = document.getElementById("F4");
+  audio.currentTime = 0;
+  audio.play();
+}
+function playFsh4() {
+  let audio = document.getElementById("Fsh4");
+  audio.currentTime = 0;
+  audio.play();
+}
+function playG4() {
+  let audio = document.getElementById("G4");
+  audio.currentTime = 0;
+  audio.play();
+}
+function playAb4() {
+  let audio = document.getElementById("Ab4");
+  audio.currentTime = 0;
+  audio.play();
+}
+function playA4() {
+  let audio = document.getElementById("A4");
+  audio.currentTime = 0;
+  audio.play();
+}
+function playBb4() {
+  let audio = document.getElementById("Bb4");
+  audio.currentTime = 0;
+  audio.play();
+}
+function playB4() {
+  let audio = document.getElementById("B4");
+  audio.currentTime = 0;
+  audio.play();
+}
+function playC5() {
+  let audio = document.getElementById("C5");
+  audio.currentTime = 0;
+  audio.play();
+}
